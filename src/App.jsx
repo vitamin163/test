a9718393742237d4bf7222475f8982d99ecfa713
vitamin163 <vitalik.domnin@gmail.com>
2020-07-17 13:47:45 +0400

    eslint

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './scss/App.scss';
 import image from './img/jp-valery-1052513-unsplash.png';
+import C from './C.jsx';
 
 const App = () => (
   <>
     <div className="main">Hello World!</div>
+    <C />
     <img src={image} alt='' />
   </>
 );
---
+++ src/C.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const C = () => (
+    <div>QWERTY</div>
+);
+
+export default C;
